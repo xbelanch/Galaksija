208a745383e8e84b4c5d06ac5c536115ccdc541c
Xavier Belanche <xbelanch@protonmail.com>
2021-10-07 22:34:38 +0200

    Add dump ROM memory and other minor upgrades

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # Galaksija 8-bit DIY microcomputer emulator
 
-Galaksija Emulator in C.
+Just another Galaksija Emulator (in C). Nothing special.
 
 
 ## References
 
-- [Galaksija: 1983 Yugoslav 8-bit DIY microcomputer](https://github.com/mejs/galaksija)
+### Galaksija related
+
+- [Galaksija: 1983 Yugoslav 8-bit DIY microcomputer](https://github.com/mejs/galaksija): Galaksija is a Z80A based microcomputer designed in 1983 in Belgrade (Yugoslavia, now Serbia) by Voja Antonić. This repo includes roms, programs and tools for working with this computer.
 - [Building a Galaksija, Part I: The Tech](https://blog.vladovince.com/building-a-galaksija-the-1980s-yugoslav-8-bit-microcomputer-part-i-the-tech/)
 - [Building a Galaksija, Part II: The Origin Story](https://blog.vladovince.com/building-a-galaksija-part-ii-the-origin-story/)
 - [Building a Galaksija, Part III: Audio cassettes, magazine listings and radio waves](https://blog.vladovince.com/building-a-galaksija-part-iii-audio-cassettes-magazine-listings-and-radio-waves/url)
+- [Assembly File Listing to generate 4K "ROM A" for Galaksija microcomputer](https://www.tablix.org/~avian/galaksija/rom/rom1.html): Disassembled from a binary ROM image and annotated by Tomaz Solc (tomaz.solc@tablix.org) using z80dasm (http://www.tablix.org/~avian/blog/articles/z80dasm).
+
+### Emulation in general
+
+- [chips](https://github.com/floooh/chips): A toolbox of 8-bit chip-emulators, helper code and complete embeddable system emulators in dependency-free C headers (a subset of C99 that compiles on gcc, clang and cl.exe).
--- src/main.c
+++ src/main.c
@@ -1,25 +1,43 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <string.h>
 
-int loadROM(char *filename) {
+
+uint8_t memory[0x2000]; // 8k of ROM
+
+int dumpMemory() {
+    size_t col = 0;
+    uint16_t row = 0;
+    uint32_t addr = 0;
+    fprintf(stdout, "%#04x:\t", row);
+    while (addr < (sizeof(memory) / sizeof(memory[0]))) {
+        fprintf(stdout, "%#04x ", memory[addr++]);
+        col++;
+        if (col == 10) {
+            row += col;
+            col = 0;
+            fprintf(stdout, "\n");
+            fprintf(stdout, "%#04x:\t", row);
+        }
+    }
+    return (0);
+}
+
+int loadROM(char *filename, uint32_t address) {
     FILE *fp = fopen(filename, "rb");
     if (fp == NULL) {
         perror("fopen");
         fprintf(stderr, "Error opening %s\n", filename);
         return (1);
+    } else {
+        fprintf(stdout, "Burn %s into memory starting at address %#04x:\n", filename, address);
     }
 
-    unsigned char byte;
-    size_t col = 0;
+    uint8_t byte;
+    uint8_t *ptr = &memory[address];
     while (fread(&byte, 1, sizeof(byte), fp) == 1) {
-        fprintf(stdout, "%#04x ", byte);
-
-        col++;
-        if (col == 10) {
-            col = 0;
-            fprintf(stdout, "\n");
-        }
-
+        memcpy(ptr++, &byte, sizeof(byte));
     }
 
     fclose(fp);
@@ -35,7 +53,15 @@ int main(int argc, char *argv[])
     (void) argv[0];
 
     fprintf(stdout, "Hello, Galaksija!\n");
-    loadROM("galaksija.rom");
+    loadROM("ROM_A.BIN", 0);
+    loadROM("ROM_B.BIN", 0x1000);
+    fprintf(stdout, "%#04x ", memory[0x5]);
+    fprintf(stdout, "%#04x ", memory[0xfff]);
+    fprintf(stdout, "%#04x ", memory[0x1000]);
+
+    dumpMemory();
+
+
 
     return 0;
 }
