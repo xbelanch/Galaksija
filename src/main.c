bd4612a06f22e3b8ef9ac4a37a607cf5b51b3ffc
Xavier Belanche <xbelanch@protonmail.com>
2021-10-12 19:35:51 +0200

    wip z80 cpu header

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <stdbool.h>
 #include "z80.h"
 
 uint8_t memory[8192]; // 8k of ROM
@@ -9,7 +10,7 @@ uint8_t memory[8192]; // 8k of ROM
 int dumpMemory() {
     size_t col = 0;
     uint16_t row = 0;
-    uint32_t addr = 0;
+    uint16_t addr = 0;
     fprintf(stdout, "%#04x:\t", row);
     while (addr < (sizeof(memory) / sizeof(memory[0]))) {
         fprintf(stdout, "%#04x ", memory[addr++]);
@@ -39,6 +40,14 @@ int loadROM(char *filename, uint32_t address) {
     return (0);
 }
 
+uint8_t readByte(uint16_t address) {
+    return memory[address];
+}
+
+void writeByte(uint16_t address, uint8_t data) {
+    memory[address] = data;
+}
+
 int main(int argc, char *argv[])
 {
     (void) argc;
@@ -48,6 +57,29 @@ int main(int argc, char *argv[])
     loadROM("ROM_A.BIN", 0);
     loadROM("ROM_B.BIN", 4096);
     // dumpMemory();
+
+    bool stop_emulation = 0;
+    uint32_t ticks = 0;
+
+    // Some dummy register test
+    z80_reg reg = {.word = 0x11};
+    fprintf(stdout, "%#04x\n", reg.word);
+    fprintf(stdout, "%#04x\n", reg.byte.high);
+    fprintf(stdout, "%#04x\n", reg.byte.low);
+    fprintf(stdout, "%#04x\n", reg.word & Z80_C_Flag);
+
+    z80_t cpu;
+    z80_init(&cpu, readByte, writeByte);
+    fprintf(stdout, "Read memory from CPU: %#02x\n", cpu.readByte(2));
+
+    while (!stop_emulation) {
+        z80_exec(&cpu, ticks);
+        // emulateInterrupts();
+        // emulateGraphics();
+        // emulateSound();
+        // emulateOtheSoftware();
+        // timeSincronization();
+    }
 
     return (0);
 }
--- src/z80.h
+++ src/z80.h
@@ -5,17 +5,86 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <assert.h>
+
+// Status indicator flags
+#define Z80_C_Flag (1<<0) // Carry
+#define Z80_N_Flag (1<<1) // Substract
+#define Z80_P_Flag (1<<2) // Parity or Overflow
+#define Z80_V_Flag Z80_P_Flag //
+#define Z80_F3_Flag (1<<3) // Undocumented flag, copy of bit 3
+#define Z80_H_Flag (1<<4) // Half Carry
+#define Z80_F5_Flag (1<<5) // Undocumented flag, copy of bit 5
+#define Z80_Z_Flag (1<<6) // Zero
+#define Z80_S_Flag (1<<7) // Sign
+
+typedef union {
+    uint16_t word;
+    struct {
+        uint8_t low; // low byte
+        uint8_t high; // high byte
+    } byte;
+} z80_reg;
 
 typedef struct {
-    uint16_t af, bc, de, hl, sp, pc;
-    uint16_t ix, iy;
-    uint8_t i, r;
-    uint16_t _af, _bc, _de, _hl;
+    //
+    // SP: Stack Pointer
+    z80_reg af; // Register AD (Accumulator + Flags)
+    z80_reg bc; // Register BC (B + C)
+    z80_reg de; // Register DE (D + E)
+    z80_reg hl; // Register HL (H + L)
+    uint16_t sp; // Stack Pointer
+    uint16_t pc; // Program Counter
+    uint16_t ix, iy; // Index or basr register for 8-bit immediate offsets
+    uint8_t i; // Interrupt vector base register
+    uint8_t r; // DRAM refresh counter;
+    uint8_t flags; // |S|Z|-|H|-|P/V|N|C|
+    z80_reg _af; // Alternate (or shadow) accumulator and flags
+    z80_reg _bc, _de, _hl; // Alternate (or shadow) registers
+
+    uint16_t memory_ptr;
+
+    bool halted : 1; // CPU is halted
+    uint8_t interrupt_mode;
+    bool interrupt_pending : 1; // Stores if there's a pending interrupt
+    bool nmi_pending : 1; // Stores if there's a pending nmi
+
+    uint8_t (*readByte)(uint16_t address);
+    void (*writeByte)(uint16_t address, uint8_t data);
 } z80_t;
 
 // initialize a new z80 cpu instance
-void z80_init(z80_t *cpu);
+void z80_init(z80_t *cpu, uint8_t (*r)(uint16_t), void (*w)(uint16_t, uint8_t));
 // reset an existing z80 cpu instance
 void z80_reset(z80_t *cpu);
+// execute instructions for at least 'ticks', but at least one, return executed ticks
+uint32_t z80_exec(z80_t* cpu, uint32_t num_ticks);
+
+void z80_init(z80_t *cpu, uint8_t (*r)(uint16_t addr), void (*w)(uint16_t addr, uint8_t byte)) {
+    cpu->readByte = r;
+    cpu->writeByte = w;
+    cpu->af = (z80_reg) { .word = 0x0 };
+}
+
+void z80_reset(z80_t *cpu) {
+    (void) cpu;
+    assert(false && "TODO: z80_reset()\n");
+}
+
+uint32_t z80_exec(z80_t* cpu, uint32_t num_ticks) {
+    uint32_t ticks = 0;
+    uint16_t pc = cpu->pc;
+    uint8_t opcode;
+    (void) pc;
+    (void) opcode;
+    do {
+        // fetch next opcode byte
+        assert(false && "opcode = memory[pc++];");
+        assert(false && "instruction = decode(opcode);");
+        assert(false && "execute(instruction);");
+    } while (ticks < num_ticks);
+    return 0;
+}
 
 #endif
